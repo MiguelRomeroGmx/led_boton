dbe55510ae33335694e289ff149bec5d9fbb467e
MiguelRomero <contacto@miguelromero.mx>
2018-02-10 20:52:35 -0600

    Comentarios

--- ledboton.ino
+++ ledboton.ino
@@ -2,16 +2,16 @@
 // Ejercicio led y boton
 
 
-const int led = 13;
-const int boton = 2;
+const int led = 13;  	// Constante led para referirnos al pin 13
+const int boton = 2;	// Constante botón para referirnos al pin 2
 
-int estado = 0;
+int estado = 0;   // Declaración de variables de tipo entero
 int anterior = 0;
 int encender = 0;
 
 void setup(){
-	pinMode(boton, INPUT);
-	pinMode(led, OUTPUT);
+	pinMode(boton, INPUT);	// Definimos boton (pin 2) como entrada
+	pinMode(led, OUTPUT);	// Definimos led (pin 13) como salida
 }
 
 void loop() {
